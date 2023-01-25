54407d66c9c98a3012ea20d333443916cb6c8550
ralphgehy <rgehy84@gmail.com>
2023-01-25 13:04:53 -0500

    initial commit

---
+++ src/test/java/testcases/TestAddToCartFlow.java
@@ -0,0 +1,13 @@
+package testcases;
+
+public class TestAddToCartFlow {
+
+    /*
+     * Test case:
+     * 1. Go to https://www.webstaurantstore.com/
+     * 2. Search for "stainless work table"
+     * 3. Check the search result ensuring every product has the word "Table" in its title.
+     * 4. Add the last of found items to Cart.
+     * 5. Empty Cart.
+     */
+}
